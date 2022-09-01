e1cf9b5ccb85f6a8c80944506455600a6161e0b1
elsangithub <102652357+elsangithub@users.noreply.github.com>
2022-09-01 17:41:48 +0900

    Update jam.sh

--- jam.sh
+++ jam.sh
@@ -2,137 +2,110 @@
 # Sync Jam otomatis berdasarkan bug isp by AlkhaNET
 # Extended GMT+7 by vitoharhari
 # Simplify usage and improved codes by helmiau
-# Supported VPN Tunnels: OpenClash
-	
+# Supported VPN Tunnels: OpenClash, Passwall, ShadowsocksR, ShadowsocksR++, v2ray, v2rayA, xray, Libernet, Xderm Mini, Wegare
+
 dtdir="/root/date"
 initd="/etc/init.d"
 logp="/root/logp"
 jamup2="/root/jam2_up.sh"
-jamup='/root/jamup.sh'
+jamup="/root/jamup.sh"
+nmfl="$(basename "$0")"
+scver="3.2"
 
 function nyetop() {
-	echo "jam.sh: Stopping VPN tunnels if available."
-	logger "jam.sh: Stopping VPN tunnels if available."
-	if [[ $(uci -q get openclash.config.enable) == "1" ]]; then "$initd"/openclash stop && echo "Stopping OpenClash"; fi
+	stopvpn="${nmfl}: Stopping"
+	echo -e "${stopvpn} VPN tunnels if available."
+	logger "${stopvpn} VPN tunnels if available."
+	if [[ -f "$initd"/openclash ]] && [[ $(uci -q get openclash.config.enable) == "1" ]]; then "$initd"/openclash restart && echo -e "${startvpn} OpenClash"; fi
+	if [[ -f "$initd"/passwall ]] && [[ $(uci -q get passwall.enabled) == "1" ]]; then "$initd"/passwall stop && echo -e "${stopvpn} Passwall"; fi
+	if [[ -f "$initd"/shadowsocksr ]] && [[ $(uci -q get shadowsocksr.@global[0].global_server) != "nil" ]]; then "$initd"/shadowsocksr stop && -e echo "${stopvpn} SSR++"; fi
+	if [[ -f "$initd"/v2ray ]] && [[ $(uci -q get v2ray.enabled.enabled) == "1" ]]; then "$initd"/v2ray stop && -e echo "${stopvpn} v2ray"; fi
+	if [[ -f "$initd"/v2raya ]] && [[ $(uci -q get v2raya.config.enabled) == "1" ]]; then "$initd"/v2raya stop && -e echo "${stopvpn} v2rayA"; fi
+	if [[ -f "$initd"/xray ]] && [[ $(uci -q get xray.enabled.enabled) == "1"  ]]; then "$initd"/xray stop && -e echo "${stopvpn} Xray"; fi
+	if grep -q "screen -AmdS libernet" /etc/rc.local; then ./root/libernet/bin/service.sh -ds && echo -e "${stopvpn} Libernet"; fi
+	if grep -q "/www/xderm/log/st" /etc/rc.local; then ./www/xderm/xderm-mini stop && echo -e "${stopvpn} Xderm"; fi
+	if grep -q "autorekonek-stl" /etc/crontabs/root; then echo "3" | stl && echo -e "${stopvpn} Wegare STL"; fi
 }
 
 function nyetart() {
-	echo "jam.sh: Restarting VPN tunnels if available."
-	logger "jam.sh: Restarting VPN tunnels if available."
-	if [[ $(uci -q get openclash.config.enable) == "1" ]]; then "$initd"/openclash restart && echo "Restarting OpenClash"; fi
+	startvpn="${nmfl}: Restarting"
+	echo -e "${startvpn} VPN tunnels if available."
+	logger "${startvpn} VPN tunnels if available."
+	if [[ -f "$initd"/openclash ]] && [[ $(uci -q get openclash.config.enable) == "1" ]]; then "$initd"/openclash restart && echo -e "${startvpn} OpenClash"; fi
+	if [[ -f "$initd"/passwall ]] && [[ $(uci -q get passwall.enabled) == "1" ]]; then "$initd"/passwall restart && echo -e "${startvpn} Passwall"; fi
+	if [[ -f "$initd"/shadowsocksr ]] && [[ $(uci -q get shadowsocksr.@global[0].global_server) != "nil" ]]; then "$initd"/shadowsocksr restart && echo -e "${startvpn} SSR++"; fi
+	if [[ -f "$initd"/v2ray ]] && [[ $(uci -q get v2ray.enabled.enabled) == "1" ]]; then "$initd"/v2ray restart && echo -e "${startvpn} v2ray"; fi
+	if [[ -f "$initd"/v2raya ]] && [[ $(uci -q get v2raya.config.enabled) == "1" ]]; then "$initd"/v2raya restart && echo -e "${startvpn} v2rayA"; fi
+	if [[ -f "$initd"/xray ]] && [[ $(uci -q get xray.enabled.enabled) == "1"  ]]; then "$initd"/xray restart && echo -e "${startvpn} Xray"; fi
+	if grep -q "screen -AmdS libernet" /etc/rc.local; then ./root/libernet/bin/service.sh -sl && echo -e "${startvpn} Libernet"; fi
+	if grep -q "/www/xderm/log/st" /etc/rc.local; then ./www/xderm/xderm-mini start && echo -e "${startvpn} Xderm"; fi
+	if grep -q "autorekonek-stl" /etc/crontabs/root; then echo "2" | stl && echo -e "${startvpn} Wegare STL"; fi
 }
 
 function ngecurl() {
 	curl -si "$cv_type" | grep Date > "$dtdir"
-	echo "jam.sh: Executed $cv_type as time server."
-	logger "jam.sh: Executed $cv_type as time server."
+	echo -e "${nmfl}: Executed $cv_type as time server."
+	logger "${nmfl}: Executed $cv_type as time server."
 }
 
 function sandal() {
-    day=$(cat "$dtdir" | cut -b 12-13)
-    month=$(cat "$dtdir" | cut -b 15-17)
-    year=$(cat "$dtdir" | cut -b 19-22)
-    time1=$(cat "$dtdir" | cut -b 24-25)
-    time2=$(cat "$dtdir" | cut -b 26-31)
-    
-    case $month in
+    hari=$(cat "$dtdir" | cut -b 12-13)
+    bulan=$(cat "$dtdir" | cut -b 15-17)
+    tahun=$(cat "$dtdir" | cut -b 19-22)
+    jam=$(cat "$dtdir" | cut -b 24-25)
+    menit=$(cat "$dtdir" | cut -b 26-31)
+
+    case $bulan in
         "Jan")
-           month="01"
+           bulan="01"
             ;;
         "Feb")
-            month="02"
+            bulan="02"
             ;;
         "Mar")
-            month="03"
+            bulan="03"
             ;;
         "Apr")
-            month="04"
+            bulan="04"
             ;;
         "May")
-            month="05"
+            bulan="05"
             ;;
         "Jun")
-            month="06"
+            bulan="06"
             ;;
         "Jul")
-            month="07"
+            bulan="07"
             ;;
         "Aug")
-            month="08"
+            bulan="08"
             ;;
         "Sep")
-            month="09"
+            bulan="09"
             ;;
         "Oct")
-            month="10"
+            bulan="10"
             ;;
         "Nov")
-            month="11"
+            bulan="11"
             ;;
         "Dec")
-            month="12"
+            bulan="12"
             ;;
         *)
            continue
 
     esac
 
-if [[ "$time1" == "08" ]] || [[ "$time1" == "09" ]];then
-	timeif=$(echo "${time1//0/}")
-	let a="$timeif""$gmt"
-else
-	let a="$time1""$gmt"
-fi
-#echo -e "time1 is $time1 and gmt is $gmt then total is $a" #debugging purpose
-
-    case $a in
-        "24")
-           a="00"
-            ;;
-        "25")
-           a="01"
-            ;;
-        "26")
-           a="02"
-            ;;
-        "27")
-           a="03"
-            ;;
-        "28")
-           a="04"
-            ;;
-        "29")
-           a="05"
-            ;;
-        "30")
-           a="06"
-            ;;
-        "31")
-           a="07"
-            ;;
-        "32")
-           a="08"
-            ;;
-        "33")
-           a="09"
-            ;;
-        "34")
-           a="10"
-            ;;
-        "35")
-           a="11"
-            ;;
-    esac
-
-date --set "$year"."$month"."$day"-"$a""$time2"
-echo -e "jam.sh: Set time to $year.$month.$day-$a$time2"
-logger "jam.sh: Set time to $year.$month.$day-$a$time2"
+	date -u -s "$tahun"."$bulan"."$hari"-"$jam""$menit" > /dev/null 2>&1
+	echo -e "${nmfl}: Set time to [ $(date) ]"
+	logger "${nmfl}: Set time to [ $(date) ]"
 }
 
 if [[ "$1" == "update" ]]; then
-	echo -e "jam.sh: Updating script..."
-	echo -e "jam.sh: Downloading script update..."
-	curl -sL raw.githubusercontent.com/elsangithub/sync-date-openwrt-with-bug/main/jam.sh > "$jamup"
+	echo -e "${nmfl}: Updating script..."
+	echo -e "${nmfl}: Downloading script update..."
+	curl -sL raw.githubusercontent.com/vitoharhari/sync-date-openwrt-with-bug/main/jam.sh > "$jamup"
 	chmod +x "$jamup"
 	sed -i 's/\r$//' "$jamup"
 	cat << "EOF" > "$jamup2"
@@ -148,7 +121,7 @@ EOF
 	sed -i 's/\r$//' "$jamup2"
 	chmod +x "$jamup2"
 	bash "$jamup2"
-	[[ -f "$jamup2" ]] && rm -f "$jamup2" && echo -e "jam.sh: update file cleaned up!" && logger "jam.sh: update file cleaned up!"
+	[[ -f "$jamup2" ]] && rm -f "$jamup2" && echo -e "${nmfl}: update file cleaned up!" && logger "${nmfl}: update file cleaned up!"
 elif [[ "$1" =~ "http://" ]]; then
 	cv_type="$1"
 elif [[ "$1" =~ "https://" ]]; then
@@ -157,52 +130,45 @@ elif [[ "$1" =~ [.] ]]; then
 	cv_type=http://"$1"
 else
 	echo -e "Usage: add domain/bug after script!."
-	echo -e "jam.sh: Missing URL/Bug/Domain!. Read https://github.com/vitoharhari/sync-date-openwrt-with-bug/blob/main/README.md for details."
-	logger "jam.sh: Missing URL/Bug/Domain!. Read https://github.com/vitoharhari/sync-date-openwrt-with-bug/blob/main/README.md for details."
+	echo -e "${nmfl}: Missing URL/Bug/Domain!. Read https://github.com/vitoharhari/sync-date-openwrt-with-bug/blob/main/README.md for details."
+	logger "${nmfl}: Missing URL/Bug/Domain!. Read https://github.com/vitoharhari/sync-date-openwrt-with-bug/blob/main/README.md for details."
 fi
 
 function ngepink() {
-	interval="3"
-	httping "$cv_type" -c "$interval" | grep connected > "$logp"
-	status=$(cat "$logp" | cut -b 1-9)
-  
-	if [[ "$status" =~ "connected" ]]; then
-		echo "jam.sh: Connection available, resuming task..."
-		logger "jam.sh: Connection available, resuming task..."
+	if [[ $(curl -si ${cv_type} | grep -c 'Date:') == "1" ]]; then
+		echo -e "${nmfl}: Connection to ${cv_type} is available, resuming task..."
+		logger "${nmfl}: Connection to ${cv_type} is available, resuming task..."
 	else 
-		echo "jam.sh: Connection unavailable, pinging again..."
-		logger "jam.sh: Connection unavailable, pinging again..."
+		echo -e "${nmfl}: Connection to ${cv_type} is unavailable, pinging again..."
+		logger "${nmfl}: Connection to ${cv_type} is unavailable, pinging again..."
+		sleep 3
 		ngepink
 	fi
 }
 
 if [[ ! -z "$cv_type" ]]; then
+	# Script Version
+	echo -e "${nmfl}: Script v${scver}"
+	logger "${nmfl}: Script v${scver}"
+	
+	# Runner
 	nyetop
 	ngepink
 	ngecurl
-
-	########
-	#Start Set GMT
-	if [[ "$2" =~ ^[+-][0-9]+$ ]];then
-		default_gmt="$2" # custom GMT
-	else
-		default_gmt="+7" # default GMT+7
-	fi
-	gmt=$(echo -e "$default_gmt" | sed -e 's/+/+/g' -e 's/-/-/g') # optional GMT by command: script.sh api.com -7
-	echo -e "jam.sh: GMT set to GMT$default_gmt"
-	logger "jam.sh: GMT set to GMT$default_gmt"
-	#End Set GMT
-	########
-	
 	sandal
 	nyetart
 
-	#Cleaning files
-	[[ -f "$logp" ]] && rm -f "$logp" && echo -e "jam.sh: logp cleaned up!" && logger "jam.sh: logp cleaned up!"
-	[[ -f "$dtdir" ]] && rm -f "$dtdir" && echo -e "jam.sh: tmp dir cleaned up!" && logger "jam.sh: tmp dir cleaned up!"
-	[[ -f "$jamup2" ]] && rm -f "$jamup2" && echo -e "jam.sh: update file cleaned up!" && logger "jam.sh: update file cleaned up!"
+	# Cleaning files
+	[[ -f "$logp" ]] && rm -f "$logp" && echo -e "${nmfl}: logp cleaned up!" && logger "${nmfl}: logp cleaned up!"
+	[[ -f "$dtdir" ]] && rm -f "$dtdir" && echo -e "${nmfl}: tmp dir cleaned up!" && logger "${nmfl}: tmp dir cleaned up!"
+	[[ -f "$jamup2" ]] && rm -f "$jamup2" && echo -e "${nmfl}: update file cleaned up!" && logger "${nmfl}: update file cleaned up!"
 else
 	echo -e "Usage: add domain/bug after script!."
-	echo "jam.sh: Missing URL/Bug/Domain!. Read https://github.com/vitoharhari/sync-date-openwrt-with-bug/blob/main/README.md for details."
-	logger "jam.sh: Missing URL/Bug/Domain!. Read https://github.com/vitoharhari/sync-date-openwrt-with-bug/blob/main/README.md for details."
+	echo -e "${nmfl}: Missing URL/Bug/Domain!. Read https://github.com/vitoharhari/sync-date-openwrt-with-bug/blob/main/README.md for details."
+	logger "${nmfl}: Missing URL/Bug/Domain!. Read https://github.com/vitoharhari/sync-date-openwrt-with-bug/blob/main/README.md for details."
 fi
+
+# Sync Jam otomatis berdasarkan bug isp by AlkhaNET
+# Extended GMT+7 by vitoharhari
+# Simplify usage and improved codes by helmiau
+# Supported VPN Tunnels: OpenClash, Passwall, ShadowsocksR, ShadowsocksR++, v2ray, v2rayA, xray, Libernet, Xderm Mini, Wegare
